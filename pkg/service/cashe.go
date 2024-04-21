224a904e96c3c6e3d369c4df9c24abebb04f9827
Maxim <grigorievma9461@gmail.com>
2024-04-21 18:03:39 +0700

    add cash

---
+++ pkg/service/cashe.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"fmt"
+	wbtechl0 "wb-tech-l0"
+)
+
+type Cache struct {
+	cache map[string]wbtechl0.Order
+}
+
+func NewCache() *Cache {
+	return &Cache{cache: make(map[string]wbtechl0.Order)}
+}
+
+func (c *Cache) GetOrder(OrderUid string) (*wbtechl0.Order, error) {
+	if order, status := c.cache[OrderUid]; status {
+		return &order, nil
+	}
+
+	return nil, fmt.Errorf("order not found")
+}
+
+func (c *Cache) AddOrder(order wbtechl0.Order) {
+	c.cache[order.OrderUid] = order
+}
